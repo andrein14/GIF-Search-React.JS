5d737412753bb8129f9b4a979eb9d9ab1f42fe20
AndreiN14 <andrei.nemes14@yahoo.com>
2019-01-23 13:09:25 +0200

    Updates

---
+++ precache-manifest.de0e8207e556e901e63a29fc531afcee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "667dc0f045efb7d0dce3",
+    "url": "/GIF-Search-React.JS/static/js/runtime~main.667dc0f0.js"
+  },
+  {
+    "revision": "1ba742cad88fc8b67437",
+    "url": "/GIF-Search-React.JS/static/js/main.1ba742ca.chunk.js"
+  },
+  {
+    "revision": "404c546e05e8d2e7e498",
+    "url": "/GIF-Search-React.JS/static/js/1.404c546e.chunk.js"
+  },
+  {
+    "revision": "1ba742cad88fc8b67437",
+    "url": "/GIF-Search-React.JS/static/css/main.3446a94a.chunk.css"
+  },
+  {
+    "revision": "2543a5ada71cecf93f720e3c398299af",
+    "url": "/GIF-Search-React.JS/index.html"
+  }
+];
